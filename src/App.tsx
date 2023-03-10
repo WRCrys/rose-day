1dbd878d1822efa41c80077ab5fcd562f8e09e34
Crystyano Almeida <ext.crystyano.almeida@zamp.com.br>
2023-03-10 00:35:21 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import rotate from './assets/rotate-phone.png'
+import { Rose } from './components/rose';
+
+const getOrientation = () =>
+  window.screen.orientation.type;
 
 function App() {
+
+  const [orientation, setOrientation] = useState<string>(getOrientation());
+  const [showMessage, setShowMessage] = useState<boolean>(false);
+
+  const updateOrientation = () => {
+    setOrientation(getOrientation())
+  }
+
+  useEffect(() => {
+    window.addEventListener(
+      'orientationchange',
+      updateOrientation
+    )
+    return () => {
+      window.removeEventListener(
+        'orientationchange',
+        updateOrientation
+      )
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* {
+        orientation !== 'landscape-primary' ?
+          <>
+            <div className='div-orientation'>
+              <img src={rotate} alt="" style={{ height: 150 }} />
+              <p>Gire seu telefone para uma <span>melhor experiência!</span></p>
+            </div>
+          </> :
+          <>
+            {showMessage ?
+              <>
+                <Rose />
+              </> :
+              <>
+                <div className="container">
+                  <div className="wrapper">
+                    <div className="static-txt">
+                      Do you
+                    </div>
+                    <ul className="dynamic-txts">
+                      <li>
+                        <span>think the surprise is over?</span>
+                      </li>
+                    </ul>
+                  </div>
+                  <button onClick={() => setShowMessage(!showMessage)}>
+                    See more
+                  </button>
+                </div>
+              </>
+            }
+          </>
+      } */}
+      {showMessage ?
+              <>
+                <Rose />
+              </> :
+              <>
+                <div className="container">
+                  <div className="wrapper">
+                    <div className="static-txt">
+                      Any surprise is not
+                    </div>
+                    <ul className="dynamic-txts">
+                      <li>
+                        <span>lated for you</span>
+                      </li>
+                    </ul>
+                  </div>
+                  <button onClick={() => setShowMessage(!showMessage)}>
+                    See more
+                  </button>
+                </div>
+              </>}
+    </>
   );
 }
 
---
+++ src/components/rose/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import './style.css'
+
+export function Rose() {
+    return (
+        <div className="main-container">
+            <div className="container">
+                <div className="glass">
+                    <div className="shine"></div>
+                </div>
+                <div className="thorns">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="leaves">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="petals">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="deadPetals">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+
+            <h3>É com imenso carinho que vos escrevo tal texto. Você mulher, tem uma missão importante na qual talvez jamais parou para refletir ou dar o devido valor. Sabia que tu és merecedora de todo amor, carinho, gratidão e um mundo de sentimentos.
+                Todos tem a sua importância mas você tem um lugar especial no tal lugar chamado palco da vida. Então dance, comemore, alegre-se e dê valor a cada momento seu. Você é importante, jamais deixe a luz que há dentro de ti se apagar muito menos deixar outra te ofuscar. Ao seu valor, ele está dentre os melhores que esse universo pode reservar, não tenha dúvida disso. Alegre-se mais uma vez, não é porque uma data foi marcada no calendário que
+                se possa esquecer dos outros dias, todo dia é o seu dia. Nunca esqueça de quem és, a mulher incrível para quem escrevo.</h3>
+
+        </div>
+    );
+}
